85ef4f179c54dd4bd7ce9202c5ae705662d15e3a
tran3tin <tran3tin@gmail.com>
2026-01-10 10:07:15 +0700

    fix: add evaluation_period to create payload to fix database error

--- backend/src/controllers/evaluationController.js
+++ backend/src/controllers/evaluationController.js
@@ -437,6 +437,7 @@ const createEvaluation = async (req, res) => {
       sister_id: sisterId,
       evaluation_type: evaluationType,
       period: evaluationPeriod,
+      evaluation_period: evaluationPeriod,
       evaluation_date: evaluationDate,
       evaluator: evaluatorId,
       spiritual_life: spiritualLife,
